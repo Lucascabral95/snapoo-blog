b65b16a6e0893e40419540739ea70d2e31f9a4f2
Lucas Cabral <lucassimple@hotmail.com>
2024-11-13 00:22:22 -0300

    Version 1.1.3

--- src/models/Usuario.tsx
+++ src/models/Usuario.tsx
@@ -1,20 +1,52 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-interface IUsuario extends mongoose.Document {
+// interface IUsuario extends mongoose.Document {
+//     email: string;
+//     password?: string;
+//     userName?: string;
+//     avatar: string;
+// }
+
+// const usuarioSchema = new mongoose.Schema<IUsuario>({
+//     email: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//     },
+//     password: {
+//         type: String
+//     },
+//     userName: {
+//         type: String,
+//         unique: true,
+//     },
+//     avatar: {
+//         type: String,
+//         default: ""
+//     },
+// });
+
+// // export default mongoose.models.Usuarios as mongoose.Model<IUsuario> || mongoose.model<IUsuario>("Usuarios", usuarioSchema);
+// const Usuarios = mongoose.models.Usuarios as mongoose.Model<IUsuario> || mongoose.model<IUsuario>("Usuarios", usuarioSchema);
+// export default Usuarios;
+
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+interface IUsuario extends Document {
     email: string;
     password?: string;
     userName?: string;
     avatar: string;
 }
 
-const usuarioSchema = new mongoose.Schema<IUsuario>({
+const usuarioSchema: Schema<IUsuario> = new Schema({
     email: {
         type: String,
         required: true,
         unique: true,
     },
     password: {
-        type: String
+        type: String,
     },
     userName: {
         type: String,
@@ -22,10 +54,11 @@ const usuarioSchema = new mongoose.Schema<IUsuario>({
     },
     avatar: {
         type: String,
-        default: ""
+        default: "",
     },
 });
 
-// export default mongoose.models.Usuarios as mongoose.Model<IUsuario> || mongoose.model<IUsuario>("Usuarios", usuarioSchema);
-const Usuarios = mongoose.models.Usuarios as mongoose.Model<IUsuario> || mongoose.model<IUsuario>("Usuarios", usuarioSchema);
+// Usamos un patrón condicional para exportar el modelo
+const Usuarios: Model<IUsuario> = mongoose.models.Usuarios || mongoose.model<IUsuario>("Usuarios", usuarioSchema);
+
 export default Usuarios;
