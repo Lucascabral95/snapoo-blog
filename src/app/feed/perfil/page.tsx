16fc9b4de20d95113ef33a26db8b06947945a5f6
Lucas Cabral <lucassimple@hotmail.com>
2024-11-13 20:35:12 -0300

    Version 1.3.5

--- src/app/feed/perfil/page.tsx
+++ src/app/feed/perfil/page.tsx
@@ -1,3 +1,224 @@
+// "use client";
+// import React, { useState, useEffect } from "react";
+// import { useSession } from "next-auth/react";
+// import Image from "next/image";
+// import { GoPlus } from "react-icons/go";
+// import SubidaImagenes from "@/components/SubidaImagenes/SubidaImagenes";
+// import axios from "axios";
+// import EstructuraImagenes from "@/components/EstructuraImagenes/EstructuraImagenes";
+// import Link from "next/link";
+// import { usePathname } from "next/navigation";
+// import toast, { Toaster } from "react-hot-toast";
+
+// interface User {
+//   id: string;
+//   name: string;
+//   email: string;
+//   image: string;
+//   saludo?: string;
+// }
+
+// interface TUser {
+//   _id: string;
+//   userName: string;
+//   email: string;
+//   avatar?: string;
+// }
+
+// const Login: React.FC = () => {
+//   const { data: session } = useSession() as { data: { user: User } | null };
+//   const [seccionSeleccionada, setSeccionSeleccionada] =
+//     useState<string>("posteos");
+//   const [posteos, setPosteos] = useState<any[]>([]);
+//   const [isOpenSubida, setIsOpenSubida] = useState<boolean>(false);
+//   const [file, setFile] = useState<File | undefined>(undefined);
+//   const [comentario, setComentario] = useState<string>("");
+//   const pathname = usePathname();
+//   const [misDatos, setMisDatos] = useState<TUser>();
+//   const [rePosteos, setRePosteos] = useState<any[]>([]);
+
+//   const enviarImagen = async () => {
+//     const form = new FormData();
+//     form.append("file", file!);
+//     form.append("comentario", comentario);
+//     form.append("id", session!.user!.id);
+
+//     try {
+//       const result = await axios.post("/api/upload", form, {
+//         headers: {
+//           "Content-Type": "multipart/form-data",
+//         },
+//       });
+
+//       if (result.status === 200 || result.status === 201) {
+//         setIsOpenSubida(false);
+//         toast.success("Imagen subida con éxito");
+//       }
+//     } catch (error) {
+//       console.log(`Se produjo un error en el servidor: ${error}`);
+//     }
+//   };
+
+//   useEffect(() => {
+//     const obtenerImagenes = async () => {
+//       try {
+//         const results = await axios.get(`/api/posteos`);
+
+//         if (results.status === 200 || results.status === 201) {
+//           const posteosFiltrados = results.data.result.filter(
+//             (posteo: any) => posteo.usuario._id === session?.user?.id
+//           );
+//           setPosteos(posteosFiltrados.reverse());
+//         }
+//       } catch (error) {
+//         console.log(`Se produjo un error en el servidor: ${error}`);
+//       }
+//     };
+
+//     if (session?.user?.id) {
+//       obtenerImagenes();
+//     }
+//   }, [session?.user?.id, isOpenSubida]);
+
+//   useEffect(() => {
+//     const obtenerDatosPersonales = async () => {
+//       try {
+//         const results = await axios.get(`/api/register`);
+
+//         if (results.status === 200 || results.status === 201) {
+//           const filtroMio = results.data.result.filter(
+//             (posteo: any) => posteo._id === session?.user?.id
+//           );
+
+//           setMisDatos(filtroMio[0]);
+//         }
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+
+//     if(session?.user?.id) {
+//       obtenerDatosPersonales();
+//     }
+//   }, [session?.user?.id]);
+
+//   useEffect(() => {
+//     const obtenerReposteos = async () => {
+//       try {
+//         const results = await axios.get(`/api/intereses`);
+
+//         if (results.status === 200 || results.status === 201) {
+//           const misCompartidos = results.data.result.filter(
+//             (posteo: any) => posteo.user._id === session?.user?.id
+//           );
+//           setRePosteos(misCompartidos[0].rePosteos.reverse());
+//         }
+//       } catch (error: any) {
+//         if (error.response) {
+//           if (error.response.status === 404) {
+//             console.log(error.response.data.error);
+//           } else if (error.response.status === 500) {
+//             window.location.reload();
+//           } else {
+//             console.log(error.response.data.error);
+//           }
+//         }
+//       }
+//     };
+
+//     if (session?.user) {
+//       obtenerReposteos();
+//     }
+//   }, [session]);
+
+//   return (
+//     <div className="seccion-perfil">
+//       {pathname === "/feed/perfil" && (
+//         <Link href="/feed/ajustes" className="editar">
+//           <button> EDITAR </button>
+//         </Link>
+//       )}
+//       <div className="imagen-nombre-perfil">
+//         <div className="imagen-de-perfil">
+//           <Image
+//             style={{ borderRadius: "50%" }}
+//             className="img"
+//             src={session?.user?.image ?? "/img/logo-snapoo.png"}
+//             alt="Logo"
+//             width="140"
+//             height="140"
+//           />
+//         </div>
+//         <div className="titulo-username">
+//           <h2 className="font-username">{misDatos?.userName ?? "Invitado"}</h2>
+//         </div>
+//       </div>
+//       <div className="posteos-reposteos">
+//         <div className="pr">
+//           <div className="p">
+//             <div
+//               className="posteos"
+//               style={{
+//                 color: seccionSeleccionada === "posteos" ? "black" : "#c0c0c0",
+//                 borderTop:
+//                   seccionSeleccionada === "posteos"
+//                     ? "0.5px solid black"
+//                     : "0.5px solid #c0c0c05a",
+//               }}
+//               onClick={() => setSeccionSeleccionada("posteos")}
+//             >
+//               <p> POSTEOS </p>
+//             </div>
+//             <div
+//               className="posteos"
+//               style={{
+//                 color:
+//                   seccionSeleccionada === "compartidos" ? "black" : "#c0c0c0",
+//                 borderTop:
+//                   seccionSeleccionada === "compartidos"
+//                     ? "0.5px solid black"
+//                     : "0.5px solid #c0c0c05a",
+//               }}
+//               onClick={() => setSeccionSeleccionada("compartidos")}
+//             >
+//               <p> COMPARTIDOS </p>
+//             </div>
+//           </div>
+//           {pathname === "/feed/perfil" && (
+//             <div className="subida-imagenes">
+//               <div className="icono">
+//                 <GoPlus
+//                   className="icon"
+//                   onClick={() => setIsOpenSubida(true)}
+//                 />
+//               </div>
+//             </div>
+//           )}
+//         </div>
+//       </div>
+
+//       <Toaster />
+
+//       {isOpenSubida && (
+//         <SubidaImagenes
+//           setIsOpenSubida={setIsOpenSubida}
+//           publicarImagen={enviarImagen}
+//           setFile={setFile}
+//           setComentario={setComentario}
+//         />
+//       )}
+
+//       {seccionSeleccionada === "posteos" ? (
+//         <EstructuraImagenes posteos={posteos} />
+//       ) : (
+//         <EstructuraImagenes posteos={rePosteos} />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Login;
+
 "use client";
 import React, { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
@@ -26,9 +247,9 @@ interface TUser {
 }
 
 const Login: React.FC = () => {
-  const { data: session } = useSession() as { data: { user: User } | null };
-  const [seccionSeleccionada, setSeccionSeleccionada] =
-    useState<string>("posteos");
+  // const { data: session, status } = useSession() as { data: { user: User } | null };
+  const { data: session, status } = useSession() as { data: { user: User } | null, status: string };
+  const [seccionSeleccionada, setSeccionSeleccionada] = useState<string>("posteos");
   const [posteos, setPosteos] = useState<any[]>([]);
   const [isOpenSubida, setIsOpenSubida] = useState<boolean>(false);
   const [file, setFile] = useState<File | undefined>(undefined);
@@ -38,10 +259,11 @@ const Login: React.FC = () => {
   const [rePosteos, setRePosteos] = useState<any[]>([]);
 
   const enviarImagen = async () => {
+    if (!session) return; // Evita la ejecución si no hay sesión
     const form = new FormData();
     form.append("file", file!);
     form.append("comentario", comentario);
-    form.append("id", session!.user!.id);
+    form.append("id", session.user.id);
 
     try {
       const result = await axios.post("/api/upload", form, {
@@ -49,93 +271,80 @@ const Login: React.FC = () => {
           "Content-Type": "multipart/form-data",
         },
       });
-
       if (result.status === 200 || result.status === 201) {
         setIsOpenSubida(false);
         toast.success("Imagen subida con éxito");
       }
     } catch (error) {
-      console.log(`Se produjo un error en el servidor: ${error}`);
+      console.error(`Se produjo un error en el servidor: ${error}`);
     }
   };
 
   useEffect(() => {
     const obtenerImagenes = async () => {
+      if (!session?.user?.id) return;
       try {
         const results = await axios.get(`/api/posteos`);
-
         if (results.status === 200 || results.status === 201) {
           const posteosFiltrados = results.data.result.filter(
-            (posteo: any) => posteo.usuario._id === session?.user?.id
+            (posteo: any) => posteo.usuario._id === session.user.id
           );
           setPosteos(posteosFiltrados.reverse());
         }
       } catch (error) {
-        console.log(`Se produjo un error en el servidor: ${error}`);
+        console.error(`Se produjo un error en el servidor: ${error}`);
       }
     };
-
-    if (session?.user?.id) {
-      obtenerImagenes();
-    }
-  }, [session?.user?.id, isOpenSubida]);
+    obtenerImagenes();
+  }, [session, isOpenSubida]);
 
   useEffect(() => {
     const obtenerDatosPersonales = async () => {
+      if (!session?.user?.id) return;
       try {
         const results = await axios.get(`/api/register`);
-
         if (results.status === 200 || results.status === 201) {
-          const filtroMio = results.data.result.filter(
-            (posteo: any) => posteo._id === session?.user?.id
+          const filtroMio = results.data.result.find(
+            (posteo: any) => posteo._id === session.user.id
           );
-
-          setMisDatos(filtroMio[0]);
+          setMisDatos(filtroMio);
         }
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
     };
-
-    if(session?.user?.id) {
-      obtenerDatosPersonales();
-    }
-  }, [session?.user?.id]);
+    obtenerDatosPersonales();
+  }, [session]);
 
   useEffect(() => {
     const obtenerReposteos = async () => {
+      if (!session?.user) return;
       try {
         const results = await axios.get(`/api/intereses`);
-
         if (results.status === 200 || results.status === 201) {
           const misCompartidos = results.data.result.filter(
-            (posteo: any) => posteo.user._id === session?.user?.id
+            (posteo: any) => posteo.user._id === session.user.id
           );
-          setRePosteos(misCompartidos[0].rePosteos.reverse());
+          setRePosteos(misCompartidos[0]?.rePosteos.reverse() || []);
         }
       } catch (error: any) {
-        if (error.response) {
-          if (error.response.status === 404) {
-            console.log(error.response.data.error);
-          } else if (error.response.status === 500) {
-            window.location.reload();
-          } else {
-            console.log(error.response.data.error);
-          }
-        }
+        console.error(`Se produjo un error en el servidor: ${error}`);
       }
     };
-
-    if (session?.user) {
-      obtenerReposteos();
-    }
+    obtenerReposteos();
   }, [session]);
 
+  // Muestra un estado de carga hasta que la sesión esté lista
+  if (status === "loading") {
+    return <p>Cargando...</p>;
+  }
+
+  // Renderizado principal
   return (
     <div className="seccion-perfil">
       {pathname === "/feed/perfil" && (
         <Link href="/feed/ajustes" className="editar">
-          <button> EDITAR </button>
+          <button>EDITAR</button>
         </Link>
       )}
       <div className="imagen-nombre-perfil">
@@ -154,6 +363,7 @@ const Login: React.FC = () => {
         </div>
       </div>
       <div className="posteos-reposteos">
+        {/* Selección de Posteos o Compartidos */}
         <div className="pr">
           <div className="p">
             <div
@@ -167,13 +377,12 @@ const Login: React.FC = () => {
               }}
               onClick={() => setSeccionSeleccionada("posteos")}
             >
-              <p> POSTEOS </p>
+              <p>POSTEOS</p>
             </div>
             <div
               className="posteos"
               style={{
-                color:
-                  seccionSeleccionada === "compartidos" ? "black" : "#c0c0c0",
+                color: seccionSeleccionada === "compartidos" ? "black" : "#c0c0c0",
                 borderTop:
                   seccionSeleccionada === "compartidos"
                     ? "0.5px solid black"
@@ -181,7 +390,7 @@ const Login: React.FC = () => {
               }}
               onClick={() => setSeccionSeleccionada("compartidos")}
             >
-              <p> COMPARTIDOS </p>
+              <p>COMPARTIDOS</p>
             </div>
           </div>
           {pathname === "/feed/perfil" && (
@@ -196,9 +405,7 @@ const Login: React.FC = () => {
           )}
         </div>
       </div>
-
       <Toaster />
-
       {isOpenSubida && (
         <SubidaImagenes
           setIsOpenSubida={setIsOpenSubida}
@@ -207,7 +414,6 @@ const Login: React.FC = () => {
           setComentario={setComentario}
         />
       )}
-
       {seccionSeleccionada === "posteos" ? (
         <EstructuraImagenes posteos={posteos} />
       ) : (
