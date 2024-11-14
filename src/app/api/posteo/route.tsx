d0aeb62addb1f1e0a873dbaa84470dd325850361
Lucas Cabral <lucassimple@hotmail.com>
2024-11-14 14:12:34 -0300

    Version 1.5.9

--- src/app/api/posteo/route.tsx
+++ src/app/api/posteo/route.tsx
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import daoPosteos from "@/DAO/PosteosDAO";
 
-export async function GET(req) {
+export async function GET(req: Request) {
      try {
           const result = await daoPosteos.getAll();
           return NextResponse.json({ result: result }, { status: 200 });
---
+++ src/models/Posteos.tsx
@@ -0,0 +1,73 @@
+// import mongoose from "mongoose";
+
+// const posteosSchema = new mongoose.Schema({
+//     likes: {
+//         type: Number,
+//         default: 0,
+//     },
+//     usuario: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "Usuarios",
+//         required: true,
+//     },
+//     comentarios: [
+//         {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "Comentarios",
+//         },
+//     ],
+//     imagen: {
+//         type: String,
+//     },
+//     descripcion: {
+//         type: String,
+//     },
+//     fecha : {
+//         type: Date,
+//         default: Date.now
+//     }
+// });
+
+// const Posteos = mongoose.models.Posteos || mongoose.model("Posteos", posteosSchema);
+// export default Posteos;
+import mongoose, { Document, Schema } from "mongoose";
+
+interface IPosteos extends Document {
+    likes: number;
+    usuario: mongoose.Schema.Types.ObjectId;
+    comentarios: mongoose.Schema.Types.ObjectId[];
+    imagen?: string;
+    descripcion?: string;
+    fecha: Date;
+}
+
+const posteosSchema = new Schema<IPosteos>({
+    likes: {
+        type: Number,
+        default: 0,
+    },
+    usuario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Usuarios",
+        required: true,
+    },
+    comentarios: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comentarios",
+        },
+    ],
+    imagen: {
+        type: String,
+    },
+    descripcion: {
+        type: String,
+    },
+    fecha: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Posteos = mongoose.models.Posteos || mongoose.model<IPosteos>("Posteos", posteosSchema);
+export default Posteos;
