7f283c98afa1cfd70d2a4f449f96367d752e6eeb
Lucas Cabral <lucassimple@hotmail.com>
2024-11-15 10:42:31 -0300

    Version 1.6.3

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,33 +1,63 @@
-"use client";
+// "use client";
+// import EstructuraImagenes from "@/components/EstructuraImagenes/EstructuraImagenes";
+// import axios from "axios";
+// import React, { useState, useEffect } from "react";
+// import "./Feed.scss";
+
+// const Inicio: React.FC = () => {
+//   const [posteos, setPosteos] = useState<any[]>([]);
+
+//   useEffect(() => {
+//     const getImages = async () => {
+//       try {
+//         const result = await axios.get(`/api/posteo`);
+
+//         if (result.status === 200 || result.status === 201) {
+//           setPosteos(result.data.result.reverse());
+//         }
+//       } catch (error: any) {
+//         if(error.response.status === 500) {
+//           window.location.reload();
+//         } else {
+//           console.log(error.response.data.error);
+//         }
+//       }
+//     };
+
+//     if(posteos.length === 0) {
+//       getImages();
+//     }
+//   }, [posteos.length]);
+
+//   return (
+//     <div className="seccion-perfil seccion-perfil-inicio seccion-array-de-imagenes">
+//       <EstructuraImagenes posteos={posteos} />
+//     </div>
+//   );
+// };
+
+// export default Inicio;
+
 import EstructuraImagenes from "@/components/EstructuraImagenes/EstructuraImagenes";
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Feed.scss";
 
-const Inicio: React.FC = () => {
-  const [posteos, setPosteos] = useState<any[]>([]);
+const obtenerImagenes = async () => {
+  try {
+    const results = await axios.get(`${process.env.NEXTAUTH_URL}/api/posteo`);
 
-  useEffect(() => {
-    const getImages = async () => {
-      try {
-        const result = await axios.get(`/api/posteo`);
-
-        if (result.status === 200 || result.status === 201) {
-          setPosteos(result.data.result.reverse());
-        }
-      } catch (error: any) {
-        if(error.response.status === 500) {
-          window.location.reload();
-        } else { 
-          console.log(error.response.data.error);
-        }
-      }
-    };
-
-    if(posteos.length === 0) {
-      getImages();
+    if (results.status === 200 || results.status === 201) {
+      return results.data.result.reverse();
     }
-  }, [posteos.length]);
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+const Inicio: React.FC = async () => {
+  const posteos = await obtenerImagenes();
 
   return (
     <div className="seccion-perfil seccion-perfil-inicio seccion-array-de-imagenes">
